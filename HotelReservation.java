9fbcef34c865771846806701a09e366b538647e2
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-08-11 16:29:43 +0530

    Adding Hotels in Hotel Reservation System

--- HotelReservation.java
+++ HotelReservation.java
@@ -1,10 +1,27 @@
 package com.HotelReservation;
 
+import java.util.ArrayList;
+
 public class HotelReservation {
 
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to the Hotel Reservation Program..");
-		
+		ArrayList<Hotel> hotelList = new ArrayList<Hotel>();
+
+		//Adding HotelNames and Rate for the Regular customer
+		//Rates are in Dollar($)
+		hotelList.add(new Hotel("LakeWood", 110));
+
+		hotelList.add(new Hotel("BridgeWood", 160));
+
+		hotelList.add(new Hotel("RidgeWood", 220));
+		int n=1;
+		System.out.println("Hotel details are :-");
+		for (Hotel hotels : hotelList) {
+			
+			System.out.println(n+")"+hotels);
+			n++;
+		}
 	}
 }
