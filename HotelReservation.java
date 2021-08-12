fb38b56389f219bde2e3aea35ec9776bccd571c5
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-08-12 17:01:41 +0530

    Cheapest best hotel for Reward Customer

--- HotelReservation.java
+++ HotelReservation.java
@@ -8,20 +8,72 @@ import java.util.Scanner;
 
 public class HotelReservation {
 	
+
+	public static void findChepestBestRatedHotel(ArrayList<Hotel> hotelList) {
+		System.out.println("Enter the CustomerType");
+		System.out.println("Press 1 - Reward Customer");
+		System.out.println("Press 2 - Regular Customer");
+		 Scanner sc = new Scanner(System.in);
+		int option = sc.nextInt();
+		switch (option) {
+		case 1:
+			try {
+				 Scanner scan = new Scanner(System.in);
+				System.out.println("Enter the starting date in this format 'YYYY-MM-DD'");
+				String localDate1 = scan.nextLine();
+				System.out.println("Enter the ending date in this format 'YYYY-MM-DD'");
+				String localDate2 = scan.nextLine();
+				LocalDate startingDate = LocalDate.parse(localDate1);
+				LocalDate endingDates = LocalDate.parse(localDate2);
+				long noOfDaysBetween = ChronoUnit.DAYS.between(startingDate, endingDates);
+
+				Hotel hotel = hotelList.stream().min(Comparator.comparing(Hotel::getRateForRewardCustomer)).get();
+				int TotalRate = (int) (hotel.getRateForRewardCustomer() * noOfDaysBetween);
+				System.out.println("The cheapest best rated Hotel name is " + hotel.getName() + " And it's rating is "
+						+ hotel.getRating());
+				System.out.println("Total Rate for " + noOfDaysBetween + " days is = " + TotalRate);
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+			break;
+		case 2:
+			try {
+				Scanner sca = new Scanner(System.in);
+				System.out.println("Enter the starting date in this format 'YYYY-MM-DD'");
+				String localDate1 = sca.nextLine();
+				System.out.println("Enter the ending date in this format 'YYYY-MM-DD'");
+				String localDate2 = sca.nextLine();
+				LocalDate startingDate = LocalDate.parse(localDate1);
+				LocalDate endingDates = LocalDate.parse(localDate2);
+				long noOfDaysBetween = ChronoUnit.DAYS.between(startingDate, endingDates);
+
+				Hotel hotel = hotelList.stream().min(Comparator.comparing(Hotel::getRateForRegularCustomer)).get();
+				int TotalRate = (int) (hotel.getRateForRegularCustomer() * noOfDaysBetween);
+				System.out.println("The cheapest best rated Hotel name is " + hotel.getName() + " And it's rating is "
+						+ hotel.getRating());
+				System.out.println("Total Rate for " + noOfDaysBetween + " days is = " + TotalRate);
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+			break;
+		}
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Hotel Reservation Program..");
 		ArrayList<Hotel> hotelList = new ArrayList<Hotel>();
-		// Adding HotelNames, weekDayRate, weekEndRate, Rating, rateForRegularCustomer,and rateForRewardCustomer
+		// Adding HotelNames, weekDayRate, weekEndRate, Rating,
+		// rateForRegularCustomer,and rateForRewardCustomer
 		// Rates are in Dollar($)
-		hotelList.add(new Hotel("LakeWood", 110, 90,3,80,80));
-		hotelList.add(new Hotel("BridgeWood", 150, 50,4,110,50));
-		hotelList.add(new Hotel("RidgeWood", 220, 150,5,100,40));
+		hotelList.add(new Hotel("LakeWood", 110, 90, 3, 80, 80));
+		hotelList.add(new Hotel("BridgeWood", 150, 50, 4, 110, 50));
+		hotelList.add(new Hotel("RidgeWood", 220, 150, 5, 100, 40));
 		int n = 1;
 		System.out.println("Hotel details are :-");
 		for (Hotel hotels : hotelList) {
 			System.out.println(n + ")" + hotels);
 			n++;
 		}
-			
+		findChepestBestRatedHotel(hotelList);
 	}
 }
